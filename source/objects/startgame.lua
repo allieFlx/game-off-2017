09804ce9a705059ad582a8950e5770888bcb85dd
allieFlx <alliefleur@outlook.com>
2017-11-05 21:42:10 +0000

    Sprites now 4 times as large

--- source/objects/startgame.lua
+++ source/objects/startgame.lua
@@ -9,14 +9,14 @@ function startgame:new()
 	player0.sprite = sodapop.newAnimatedSprite(0,0)
 	player0.sprite:addAnimation('walk', {
 		image = love.graphics.newImage 'resources/bb_walk_sheet.png',
-		frameWidth = 32,
-		frameHeight = 32,
+		frameWidth = 128,
+		frameHeight = 128,
 		frames = {
 		{1,1,2,1,.2},
 		},
 	})
 
-	self.area:addGameObject('Entity', 20, 20, {player = true, sprite = player0.sprite})
+	self.area:addGameObject('Entity', 300, 300, {player = true, sprite = player0.sprite})
 end
 
 function startgame:update(dt)
